46932253e5ca19e37334ec7a90f22dd9fe70b90c
Idris AY <97283155+idrisay@users.noreply.github.com>
2023-03-26 19:14:37 +0200

    feat: user name

--- src/components/MessageInput.js
+++ src/components/MessageInput.js
@@ -2,15 +2,17 @@ import { useState } from "react";
 import { db } from "../utils/firebase";
 
 import { getDatabase, ref, child, push, update } from "firebase/database";
+import { useAppContext } from "@/utils/context";
 
 const MessageInput = () => {
   const [message, setMessage] = useState("");
+  const { user } = useAppContext();
 
   const handleSendMessage = () => {
     const db = getDatabase();
     const postData = {
       message: message,
-      user_id: 123,
+      user_id: `${user.firstName} ${user.lastName}`,
     };
 
     const newPostKey = push(child(ref(db), "messages")).key;
